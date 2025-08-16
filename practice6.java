cb8d5c6f7e1a2a1b25885d5e9c7f28cd6208694f
Ritchie Boy Romagos <ritchiebromagos7@gmail.com>
2025-08-16 15:13:34 +0800

    new

--- practice6.java
+++ practice6.java
@@ -1,7 +1,7 @@
-public class practice6 {
+public class practice6{
     public static void main(String[] args) {
         for(int r = 1; r <= 3; r++){
-            for(int s = 1; s <= 3 - r; s++){
+            for(int s = r; s <= 3 -1; s++){
                 System.out.print(" ");
             }
             for(int k = 1; k <= r * 2 - 1; k++){
